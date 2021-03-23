733bc1bdb4fd99b65b39eee4fdc98f4310bf474d
Dongak Bair <bayer07@inbox.ru>
2021-03-23 22:38:11 +0300

    Add domain

---
+++ Students.Domain/Students.Domain/Models/Group.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Students.Domain.Models
+{
+    public class Group<TKey>
+    {
+        [Key]
+        public TKey Id { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Name { get; set; }
+    }
+}
---
+++ Students.Domain/Students.Domain/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Students.Domain.Enums;
+
+namespace Students.Domain.Models
+{
+    public class Student<TKey>
+    {
+        [Key]
+        public TKey Id { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string FirstName { get; set; }
+
+        [MaxLength(60)]
+        public string Patronymic { get; set; }
+
+        [MinLength(6)]
+        [MaxLength(16)]
+        public string UniqIdentity { get; set; }
+
+    }
+}
